f525c1c9b68c718e87ed6126200593ab46046ae2
Will Pink <will.pink@gmail.com>
2019-04-03 18:09:51 +0200

    chore: Prometheus Operator

---
+++ modules/helm/prometheus/main.tf
@@ -0,0 +1,17 @@
+data "template_file" "values" {
+  template = "${file("${path.module}/files/values.tpl.yaml")}"
+  vars = {
+    domain = "${var.domain}"
+    project = "${var.project}"
+  }
+}
+
+resource "helm_release" "prometheus-operator" {
+  name      = "prometheus-operator"
+  chart     = "stable/prometheus-operator"
+  namespace = "${var.namespace}"
+  keyring   = ""
+  values = [
+    "${data.template_file.values.rendered}"
+  ]
+}
