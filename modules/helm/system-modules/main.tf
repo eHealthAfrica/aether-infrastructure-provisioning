96cde02847e56e3227fcaf79610d7b48621bc4f0
Will Pink <will.pink@gmail.com>
2019-04-03 18:03:47 +0200

    chore: monitoring parameters

--- modules/helm/system-modules/main.tf
+++ modules/helm/system-modules/main.tf
@@ -3,6 +3,7 @@ module "dashboard" {
   source = "../kubernetes-dashboard"
 }
 
+# external DNS GCP DNS
 module "external-dns" {
   source = "../external-dns"
   domain = "${var.domain}"
@@ -14,8 +15,16 @@ module "nginx" {
   source = "../nginx-ingress-controller"
 }
 
+# Cert-manager letsencrypt
 module "cert-manager" {
   source = "../cert-manager"
   domain = "${var.domain}"
   google_project = "${var.google_project}"
+}
+
+# Prometheus monitoring
+module "prometheus" {
+  source = "../prometheus"
+  domain = "${var.domain}"
+  project = "${var.project}"
 }
