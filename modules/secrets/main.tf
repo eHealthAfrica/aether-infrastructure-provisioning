5de1bed576673b8b2c60687faceaa14da6a4ad61
Will Pink <will.pink@gmail.com>
2019-04-03 18:01:27 +0200

    chore: add htauth for monitoring

--- modules/secrets/main.tf
+++ modules/secrets/main.tf
@@ -11,11 +11,11 @@ resource "kubernetes_secret" "google-application-credentials" {
 # service account for bucket acccess
 resource "kubernetes_secret" "google-bucket-credentials" {
   metadata {
-    name = "${var.namespace}-bucket-credentials"
+    name = "bucket-credentials"
     namespace = "${var.namespace}"
   }
   data {
-    credentials.json = "${var.bucket_credentials}"
+    credentials.json = "${base64decode(var.bucket_credentials)}"
   }
 }
 
@@ -30,4 +30,26 @@ resource "kubernetes_secret" "db_password" {
     user = "${var.postgres_root_username}"
     password = "${var.postgres_root_password}"
   }
+}
+
+resource "kubernetes_namespace" "monitoring" {
+  metadata {
+    name = "${var.monitoring_namespace}"
+  }
+}
+
+resource "kubernetes_secret" "monitoring" {
+  metadata {
+    name = "basic-auth"
+    namespace = "monitoring"
+  }
+
+  data {
+    username = "${var.monitoring_user}"
+    password = "${var.monitoring_password}"
+  }
+
+  type = "kubernetes.io/basic-auth"
+
+  depends_on = ["kubernetes_namespace.monitoring"]
 }
